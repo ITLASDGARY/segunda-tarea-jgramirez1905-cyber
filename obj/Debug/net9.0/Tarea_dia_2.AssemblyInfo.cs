e704180b67dff10b60ce495056b1a12a08fd97c0
jgramirez1905-cyber <jgramirez1905@gmail.com>
2026-01-30 01:18:59 +0000

    jeremy

--- obj/Debug/net9.0/Tarea_dia_2.AssemblyInfo.cs
+++ obj/Debug/net9.0/Tarea_dia_2.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Tarea_dia_2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bb58f9882ad462b85185bcf266c8bd169409adbc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+20e700a3dd6f116a0832e012fa8c5b2d3fd04d40")]
 [assembly: System.Reflection.AssemblyProductAttribute("Tarea_dia_2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Tarea_dia_2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
